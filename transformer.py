6f05f22d2ba0162bdd7a0b3d65af8a3230c0d9ca
James Ma <jamesma100@gmail.com>
2025-04-30 16:01:40 -0700

    clean up and add masking

--- transformer.py
+++ transformer.py
@@ -1,11 +1,23 @@
+from tokenizer import Tokenizer
+from typing import List, Dict
+
+import csv
+import json
+import math
+import pandas as pd
 import torch
 import torch.nn as nn
-import math
-import csv
-from tokenizer import Tokenizer
-from collections.abc import Callable
+import torch.nn.functional as F
 
-from typing import List, Dict
+"""
+Size defs
+
+N         Number of tokens in input sentence
+D_MODEL   Size of embedding of each token
+H         Number of attention heads
+D_HEAD    Size of attention head, equal to D_MODEL / H
+BATCH_SZ  Size of batch
+"""
 
 
 class Embedding(nn.Module):
@@ -15,151 +27,225 @@ class Embedding(nn.Module):
     # are in the input. Doing so, however, means you need to rescale your input s.t.
     # each token lie in range [0, length of compressed vocab - 1] as opposed to
     # [0, length of full vocab - 1] otherwise it will not be a valid embedding
-    def __init__(self, vocab_sz: int, d_model: int):
+    def __init__(self, vocab_sz, d_model):
         super(Embedding, self).__init__()
         self.d_model = d_model
         self.embedding = nn.Embedding(vocab_sz, d_model)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         return self.embedding(X) * math.sqrt(self.d_model)
 
+
 class PosEnc(nn.Module):
-    def __init__(self, n: int, d_model: int, base: int = 10000, p_drop: float = 0.1):
+    def __init__(self, n, d_model, base=10000, p_drop=0.1):
         super(PosEnc, self).__init__()
-        # TODO: make this not dumb
+        self.n = n
         self.PE = torch.zeros(n, d_model)
         for pos in range(n):
             for i in range(d_model // 2):
-                denom = math.pow(10000, 2*i/d_model)
-                self.PE[pos][2*i] = math.sin(pos / denom)
-                self.PE[pos][2*i+1] = math.cos(pos / denom)
+                denom = math.pow(10000, 2 * i / d_model)
+                self.PE[pos][2 * i] = math.sin(pos / denom)
+                self.PE[pos][2 * i + 1] = math.cos(pos / denom)
         self.dropout = nn.Dropout(p=p_drop)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         return X + self.dropout(self.PE)
 
 
 class LayerNorm(nn.Module):
-    def __init__(self, n: int, d_model: int, eps: float = 1e-5):
+    def __init__(self, n, d_model, eps=1e-5):
         super(LayerNorm, self).__init__()
-        self.n = n
-        self.d_model = d_model
         self.eps = eps
         self.alpha = nn.Parameter(torch.ones(n, d_model))
         self.beta = nn.Parameter(torch.zeros(n, d_model))
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         mean = X.mean(1, keepdim=True)
         var = X.var(1, keepdim=True, unbiased=False)
         return (X - mean) / torch.sqrt(var + self.eps) * self.alpha + self.beta
 
 
 class ResidualConnection(nn.Module):
-    def __init__(self, p_drop: float = 0.1):
+    def __init__(self, p_drop=0.1):
         super(ResidualConnection, self).__init__()
         self.dropout = nn.Dropout(p=p_drop)
 
-    def forward(self, X: torch.Tensor, sublayer: Callable[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+    def forward(self, X, sublayer):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N, D_MODEL)
+        sublayer: function mapping tensor -> tensor, both of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         return X + self.dropout(sublayer(X))
 
+
 class AddAndNorm(nn.Module):
-    def __init__(self, n: int, d_model: int, eps: float =1e-5, p_drop: float = 0.1):
+    def __init__(self, n, d_model, eps=1e-5, p_drop=0.1):
         super(AddAndNorm, self).__init__()
         self.add = ResidualConnection()
         self.norm = LayerNorm(n, d_model, eps)
         self.dropout = nn.Dropout(p=p_drop)
 
-    def forward(self, X: torch.Tensor, sublayer: Callable[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+    def forward(self, X, sublayer):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N, D_MODEL)
+        sublayer: function mapping tensor -> tensor, both of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         return self.norm(self.add(X, sublayer))
 
 
 class FFN(nn.Module):
-    """
-    y = xAT + b
-    Input: X(nx512), W1(512x2048), W2(2048x512), b1(1x2048), b2(1x512)
-    Output: max{0, (xW1 + b1)}W2 + b2 (nx512)
-    """
-    def __init__(self, n: int, d_model: int, dff: int):
+    def __init__(self, n, d_model, dff):
         super(FFN, self).__init__()
         self.W1 = nn.Linear(d_model, dff)
         self.W2 = nn.Linear(dff, d_model)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         return self.W2(self.W1(X).relu())
 
 
+# TODO add masking to encoder to skip padding
+# TODO add masking to decoder to skip later tokens
 class MultiheadedAttention(nn.Module):
-    def __init__(self, n: int, h: int, d_model: int):
+    def __init__(self, n, h, d_model, mask=False):
         super(MultiheadedAttention, self).__init__()
         self.n = n
         self.h = h
-        self.d_model = d_model
         self.d_q = d_model // h
         self.d_k = d_model // h
         self.d_v = d_model // h
         self.queries = [nn.Linear(d_model, self.d_q) for i in range(h)]
         self.keys = [nn.Linear(d_model, self.d_k) for i in range(h)]
         self.values = [nn.Linear(d_model, self.d_v) for i in range(h)]
+        self.mask = mask
 
-    def attention(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:
-        QKT = Q.matmul(K.transpose(0, 1))
-        return (QKT / math.sqrt(self.d_k)).softmax(dim=1).matmul(V)
+    def attention(self, Q, K, V):
+        """
+        Args:
+        Q: tensor of size (BATCH_SZ, N, D_HEAD)
+        K: tensor of size (BATCH_SZ, N, D_HEAD)
+        V: tensor of size (BATCH_SZ, N, D_HEAD)
+
+        Output: tensor of size (BATCH_SZ, N, D_HEAD)
+        """
+        QKT = Q.matmul(K.transpose(1, 2))
+        if self.mask:
+            indices = torch.triu_indices(self.n, self.n, offset=1)
+            QKT[:, indices[0], indices[1]] = float("-inf")
+
+        return (QKT / math.sqrt(self.d_k)).softmax(dim=2).matmul(V)
 
     # NOTE: X_Q = X_K = X_V if self-attention
-    def forward(self, X_Q: torch.Tensor, X_K: torch.Tensor, X_V: torch.Tensor) -> torch.Tensor:
+    def forward(self, X_Q, X_K, X_V):
+        """
+        Args:
+        X_Q: tensor of size (BATCH_SZ, N, D_MODEL)
+        X_K: tensor of size (BATCH_SZ, N, D_MODEL)
+        X_V: tensor of size (BATCH_SZ, N, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N, D_MODEL)
+        """
         Qs = [self.queries[i](X_Q) for i in range(self.h)]
         Ks = [self.keys[i](X_K) for i in range(self.h)]
         Vs = [self.values[i](X_V) for i in range(self.h)]
         heads = [self.attention(Q, K, V) for Q, K, V in zip(Qs, Ks, Vs)]
-        return torch.cat(heads, dim=1)
+        return torch.cat(heads, dim=2)
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, n: int, h: int, d_model: int):
+    def __init__(self, n, h, d_model):
         super(EncoderLayer, self).__init__()
-        self.n = n
-        self.h = h
-        self.d_model = d_model
         self.layer1 = MultiheadedAttention(n, h, d_model)
         self.add_and_norm1 = AddAndNorm(n, d_model)
         self.layer2 = FFN(n, d_model, d_model * 4)
         self.add_and_norm2 = AddAndNorm(n, d_model)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+        """
         attention_out = self.add_and_norm1(X, lambda x: self.layer1(x, x, x))
         ffn_out = self.add_and_norm2(attention_out, lambda x: self.layer2(x))
         return ffn_out
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, n: int, h: int, d_model: int, Z_e: torch.Tensor):
+    def __init__(self, n, h, d_model, Z_e):
+        """
+        Args:
+        Z_e: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+            Represents the output of the encoder
+        """
         super(DecoderLayer, self).__init__()
-        self.n = n
-        self.h = h
-        self.d_model = d_model
         self.Z_e = Z_e
 
-        self.layer1 = MultiheadedAttention(n, h, d_model)
+        self.layer1 = MultiheadedAttention(n, h, d_model, mask=True)
         self.add_and_norm1 = AddAndNorm(n, d_model)
         self.layer2 = MultiheadedAttention(n, h, d_model)
         self.add_and_norm2 = AddAndNorm(n, d_model)
         self.layer3 = FFN(n, d_model, d_model * 4)
         self.add_and_norm3 = AddAndNorm(n, d_model)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N_TGT, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N_TGT, D_MODEL)
+        """
         self_attention_out = self.add_and_norm1(X, lambda x: self.layer1(x, x, x))
-        cross_attention_out = self.add_and_norm2(self_attention_out, lambda x: self.layer2(x, self.Z_e, self.Z_e))
+        cross_attention_out = self.add_and_norm2(
+            self_attention_out, lambda x: self.layer2(x, self.Z_e, self.Z_e)
+        )
         ffn_out = self.add_and_norm3(cross_attention_out, lambda x: self.layer3(x))
         return ffn_out
 
 
 class Encoder(nn.Module):
-    def __init__(self, n: int, h: int, d_model: int, num_layers: int):
+    def __init__(self, n, h, d_model, num_layers):
         super(Encoder, self).__init__()
         self.pe = PosEnc(n, d_model)
         self.encoder_layers = [EncoderLayer(n, h, d_model) for i in range(num_layers)]
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+        """
         Z_e = self.pe(X)
         for encoder_layer in self.encoder_layers:
             Z_e = encoder_layer(Z_e)
@@ -167,48 +253,95 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, n: int, h: int, d_model: int, num_layers: int, Z_e: torch.Tensor):
+    def __init__(self, n, h, d_model, num_layers, Z_e):
+        """
+        Args:
+        Z_e: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+            Represents the output of the encoder
+        """
         super(Decoder, self).__init__()
         self.Z_e = Z_e
         self.pe = PosEnc(n, d_model)
-        self.decoder_layers = [DecoderLayer(n, h, d_model, Z_e) for i in range(num_layers)]
+        self.decoder_layers = [
+            DecoderLayer(n, h, d_model, Z_e) for i in range(num_layers)
+        ]
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
+    def forward(self, X):
+        """
+        Args:
+        X: tensor of size (BATCH_SZ, N_TGT, D_MODEL)
+
+        Output: tensor of size (BATCH_SZ, N_TGT, D_MODEL)
+        """
         Z_d = self.pe(X)
         for decoder_layer in self.decoder_layers:
             Z_d = decoder_layer(Z_d)
         return Z_d
 
-def tokenize_input(t_e: List[str], d_model: int) -> torch.Tensor:
+
+def tokenize_input(t_e: List[str], d_model):
+    """
+    Args:
+    t_e: list of sentences, with length BATCH_SZ
+
+    Output: tensor of size (BATCH_SZ, N_SRC, D_MODEL)
+        Each sentence is converted into a list of tokens, then padded to the
+        max length of the token lists (N_SRC) , then converted to an embedding
+        of size D_MODEL
+    """
     src_tokenizer = Tokenizer(t_e)
-    src_tokens, src_vocab = src_tokenizer.encode()
-    src_embedding_fn = Embedding(len(src_vocab), d_model)
-    src_embedding = src_embedding_fn(torch.tensor(src_tokens[0]))
-    return src_embedding
+    src_tokens, src_base, src_vocab = src_tokenizer.encode()
+    src_tokens = [torch.tensor(sentence) for sentence in src_tokens]
+    max_len = max([sentence.size(0) for sentence in src_tokens])
+
+    padded = [torch.zeros((1, max_len), dtype=int) for i in range(len(src_tokens))]
+    for i in range(len(padded)):
+        padded[i][:, : src_tokens[i].size(0)] = src_tokens[i]
+    cat = torch.concat(padded)
+    src_embedding_fn = Embedding(len(src_vocab) + len(src_base) + 1, d_model)
+    src_embeddings = src_embedding_fn(cat)
+    return src_embeddings
 
 
-def tokenize_output(t_d: List[str], d_model: int) -> torch.Tensor:
-    # return torch.randn(100, 512)
+def tokenize_output(t_d: List[str], d_model):
+    """
+    Args:
+    t_d: list of sentences, with length BATCH_SZ
+
+    Output: tensor of size (BATCH_SZ, N_TGT, D_MODEL)
+        Each sentence is converted into a list of tokens, then padded to the
+        max length of the token lists (N_SRC) , then converted to an embedding
+        of size D_MODEL
+    """
     tgt_tokenizer = Tokenizer(t_d)
-    tgt_tokens, tgt_vocab = tgt_tokenizer.encode()
-    tgt_embedding_fn = Embedding(len(tgt_vocab), d_model)
-    tgt_embedding = tgt_embedding_fn(torch.tensor(tgt_tokens[0]))
-    print("tgt: ", tgt_embedding)
-    return tgt_embedding
+    tgt_tokens, tgt_base, tgt_vocab = tgt_tokenizer.encode()
+    tgt_tokens = [torch.tensor(sentence) for sentence in tgt_tokens]
+    max_len = max([sentence.size(0) for sentence in tgt_tokens])
+
+    padded = [torch.zeros((1, max_len), dtype=int) for i in range(len(tgt_tokens))]
+    for i in range(len(padded)):
+        padded[i][:, : tgt_tokens[i].size(0)] = tgt_tokens[i]
+    cat = torch.concat(padded)
+    tgt_embedding_fn = Embedding(len(tgt_vocab) + len(tgt_base) + 1, d_model)
+    tgt_embeddings = tgt_embedding_fn(cat)
+    return tgt_embeddings
+
 
 # TODO(implement)
-def decoder_unembed(Z_d: torch.Tensor) -> torch.Tensor:
+def decoder_unembed(Z_d):
     return Z_d
 
-def simulate(t_e: List[str], t_d: List[str], d_model: int, h: int, num_layers: int) -> None:
-    # <Encoder start>
+
+def simulate(t_e: List[str], t_d: List[str], h, num_layers) -> None:
+    d_model = 512
     Z_e = tokenize_input(t_e, d_model)
-    n_src = Z_e.size()[0]
+    n_src = max([sentence.size(0) for sentence in Z_e])
     encoder = Encoder(n_src, h, d_model, 6)
     Z_e = encoder(Z_e)
 
     Z_d = tokenize_output(t_d, d_model)
-    n_tgt = Z_d.size()[0]
+    n_tgt = Z_d.size(0)
+    n_tgt = max([sentence.size(0) for sentence in Z_d])
     decoder = Decoder(n_tgt, h, d_model, 6, Z_e)
     Z_d = decoder(Z_d)
 
@@ -218,26 +351,30 @@ def simulate(t_e: List[str], t_d: List[str], d_model: int, h: int, num_layers: i
 
 if __name__ == "__main__":
     de, en = [], []
-    with open("./data/sample.csv", newline='') as f:
-        reader = csv.reader(f)
-        for i, l in enumerate(reader):
-            if i == 0: # skip title row
-                continue
-            de.append(l[0])
-            en.append(l[1])
+    nrows = 50
+    with open("./data/wmt14_translate_de-en_train.csv", encoding="utf-8") as f:
+        data = pd.read_csv(
+            "./data/wmt14_translate_de-en_train.csv", lineterminator="\n", nrows=nrows
+        )
+        for i, row in data.iterrows():
+            de.append(row.iloc[0])
+            en.append(row.iloc[1])
 
-    d_model = 512
+    # src_decoded = src_tokenizer.decode(src_tokens, src_base, src_vocab)
+    # assert src_decoded == de
+    # with open("./out/src_base.json", "w+") as fp:
+    #     json.dump(src_base, fp)
 
-    src_tokenizer = Tokenizer(de)
-    src_tokens, src_vocab = src_tokenizer.encode()
-    src_embedding_fn = Embedding(len(src_vocab), d_model)
-    src_embeddings = [src_embedding_fn(torch.tensor(sentence)) for sentence in src_tokens]
+    # with open("./out/src_vocab.json", "w+") as fp:
+    #     json.dump(src_vocab, fp)
 
-    tgt_tokenizer = Tokenizer(en)
-    tgt_tokens, tgt_vocab = tgt_tokenizer.encode()
-    tgt_embedding_fn = Embedding(len(tgt_vocab), d_model)
-    tgt_embeddings = [src_embedding_fn(torch.tensor(sentence)) for sentence in tgt_tokens]
+    # tgt_decoded = tgt_tokenizer.decode(tgt_tokens, tgt_base, tgt_vocab)
+    # assert tgt_decoded == en
+    # with open("./out/tgt_base.json", "w+") as fp:
+    #     json.dump(tgt_base, fp)
 
-    out = simulate([de[1]], [en[1]], d_model, 8, 6)
-    print(out.size())
+    # with open("./out/tgt_vocab.json", "w+") as fp:
+    #     json.dump(tgt_vocab, fp)
 
+    out = simulate(de, en, 8, 6)
+    print("[INFO] final output size: ", out.size())
