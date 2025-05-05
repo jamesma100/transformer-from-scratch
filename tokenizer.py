f6846aaaee34cbd6326ab4c751a747e4cfa55f26
James Ma <jamesma100@gmail.com>
2025-05-04 21:54:28 -0700

    have tokenizer take in text not list

--- tokenizer.py
+++ tokenizer.py
@@ -12,11 +12,10 @@ class Tokenizer:
         Find top occurring pair of tokens, only return if it occurs more than once, else None.
         """
         freq = {}
-        for l in tokens:
-            sz = len(l)
-            for i in range(sz - 1):
-                pair = (l[i], l[i+1])
-                freq[pair] = freq.get(pair, 0) + 1
+        sz = len(tokens)
+        for i in range(sz - 1):
+            pair = (tokens[i], tokens[i+1])
+            freq[pair] = freq.get(pair, 0) + 1
 
         top_pairs = sorted(list([k, v] for k, v in freq.items()), key=lambda x:x[1], reverse=True)
         return top_pairs[0][0] if top_pairs and top_pairs[0][1] > 1 else None
@@ -26,14 +25,11 @@ class Tokenizer:
         Reverse all unicode code points with their equivalent token idx
         """
         cp = []
-        for l in tokens:
-            tmp = []
-            for token in l:
-                if token not in reverse:
-                    # this should never happen
-                    raise AssertionError("Error: token not in reverse mapping.")
-                tmp.append(reverse[token])
-            cp.append(tmp)
+        for token in tokens:
+            if token not in reverse:
+                # this should never happen
+                raise AssertionError("Error: token not in reverse mapping.")
+            cp.append(reverse[token])
         return cp
     
     def _init_vocab(self, tokens):
@@ -44,16 +40,12 @@ class Tokenizer:
         Returns a tuple consisting of a mapping from token idx -> unicode and a reverse
         mapping from unicode -> token idx
         """
+        uniq = set(tokens)
         base = {}
         reverse = {}
-        next_idx = 1  # save 0 for padding
-        for l in tokens:
-            for c in l:
-                if c in reverse:
-                    continue
-                base[next_idx] = c
-                reverse[c] = next_idx
-                next_idx += 1
+        for i, token in enumerate(uniq):
+            base[i] = token
+            reverse[token] = i
         return (base, reverse)
 
     def _merge(self, tokens, top_pair, next_idx):
@@ -61,21 +53,18 @@ class Tokenizer:
         Replace every occurrence of top_pair with next_idx
         """
         cp = []
-        for l in tokens:
-            sz = len(l)
-            i = 0
-            tmp = []
-            while i < sz - 1:
-                pair = (l[i], l[i+1])
-                if pair == top_pair:
-                    tmp.append(next_idx)
-                    i += 2
-                else:
-                    tmp.append(l[i])
-                    i += 1
-            if i == sz - 1:
-                tmp.append(l[sz - 1])
-            cp.append(tmp)
+        sz = len(tokens)
+        i = 0
+        while i < sz - 1:
+            pair = (tokens[i], tokens[i+1])
+            if pair == top_pair:
+                cp.append(next_idx)
+                i += 2
+            else:
+                cp.append(tokens[i])
+                i += 1
+        if i == sz - 1:
+            cp.append(tokens[sz - 1])
         return cp
     
     def _get_freq(self, tokens):
@@ -97,6 +86,7 @@ class Tokenizer:
         """
         Recursively resolve each entry in vocab and print its associated string
         """
+        print("[INFO] printing generated vocab:")
         keys = sorted(vocab.keys())
         for key in keys:
             symbol = self._find_symbol(base, vocab, key)
@@ -107,14 +97,14 @@ class Tokenizer:
             print(displ)
     
     
-    def encode(self, max_len=3000):
-        tokens = [[ord(c) for c in l] for l in self.text]
+    def encode(self, max_len=100):
+        tokens = [ord(c) for c in self.text]
         base, reverse = self._init_vocab(tokens)
         tokens = self._replace(tokens, reverse)
         next_idx = max(base.keys()) + 1
         i = 1
         vocab = {}
-        initial_token_count = sum(len(sentence) for sentence in tokens)
+        initial_token_count = len(tokens)
         while len(vocab) < max_len:
             top_pair = self._find_top_pair(tokens)
             if not top_pair:
@@ -123,8 +113,8 @@ class Tokenizer:
             tokens = self._merge(tokens, top_pair, next_idx)
             next_idx += 1
             i += 1
-        final_token_count = sum(len(sentence) for sentence in tokens)
-        print("[INFO] BPE compression ratio: {}".format(final_token_count / initial_token_count))
+        final_token_count = len(tokens)
+        print("[INFO] BPE compression ratio: {}".format(1 - final_token_count / initial_token_count))
         return (tokens, base, vocab)
     
     def _decode_token(self, token, base, vocab):
@@ -135,49 +125,26 @@ class Tokenizer:
 
     def decode(self, tokens, base, vocab):
         res = []
-        for l in tokens:
-            tmp = []
-            for token in l:
-                tmp.append("".join(self._decode_token(token, base, vocab)))
-            res.append("".join(tmp))
-        return res
+        for token in tokens:
+            res.append(self._decode_token(token, base, vocab))
+        return "".join([s for li in res for s in li])
 
 
 if __name__ == "__main__":
-    nrows = 200
-    en = []
-    fr = []
-    data = pd.read_csv('./data/wmt14_translate_fr-en_train_sample.csv', lineterminator='\n', nrows=nrows)
-    for i, row in data.iterrows():
-        en.append(row.iloc[1])
-        fr.append(row.iloc[0])
+    filepath = "./data/samurai_champloo/Samurai.Champloo.2004.E02.720p.BDRip.XviD.AC3-ViSiON.srt_cleaned"
+    with open(filepath, "r") as fp:
+        content = fp.read()
 
-    tokenizer_en = Tokenizer(en)
-    tokens_en, base_en, vocab_en = tokenizer_en.encode()
-    decoded_en = tokenizer_en.decode(tokens_en, base_en, vocab_en)
-    assert decoded_en == en
+    tokenizer = Tokenizer(content)
+    tokens, base, vocab = tokenizer.encode()
+    decoded = tokenizer.decode(tokens, base, vocab)
+    assert decoded == content
 
-    with open("./out/tokens_en.txt", "w+") as fp:
-        for tokens in tokens_en:
-            fp.write("{}\n".format(",".join([str(token) for token in tokens])))
+    with open("./out/tokens.txt", "w+") as fp:
+        fp.write(",".join([str(token) for token in tokens]))
 
-    with open("./out/base_en.json", "w+") as fp:
-        json.dump(base_en, fp)
+    with open("./out/base.json", "w+") as fp:
+        json.dump(base, fp)
 
-    with open("./out/vocab_en.json", "w+") as fp:
-        json.dump(vocab_en, fp)
-
-    tokenizer_fr = Tokenizer(fr)
-    tokens_fr, base_fr, vocab_fr = tokenizer_fr.encode()
-    decoded_fr = tokenizer_fr.decode(tokens_fr, base_fr, vocab_fr)
-    assert decoded_fr == fr
-
-    with open("./out/tokens_fr.txt", "w+") as fp:
-        for tokens in tokens_fr:
-            fp.write("{}\n".format(",".join([str(token) for token in tokens])))
-
-    with open("./out/base_fr.json", "w+") as fp:
-        json.dump(base_fr, fp)
-
-    with open("./out/vocab_fr.json", "w+") as fp:
-        json.dump(vocab_fr, fp)
+    with open("./out/vocab.json", "w+") as fp:
+        json.dump(vocab, fp)
